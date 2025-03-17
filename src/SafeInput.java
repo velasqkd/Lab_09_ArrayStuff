4e630b4f0556c0119e3766d19bb657ec503b6450
velasqkd <velasqkd@mail.uc.edu>
2025-03-16 20:31:00 -0400

    Update SafeInput.java

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class SafeInput {
+    public static int getRangedInt(String prompt, int low, int high) {
+        Scanner scanner = new Scanner(System.in);
+        int input = -1;
+        while (input < low || input > high) {
+            System.out.print(prompt);
+            if (scanner.hasNextInt()) {
+                input = scanner.nextInt();
+                if (input < low || input > high) {
+                    System.out.println("Please enter a value between " + low + " and " + high + ".");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.next();
+            }
+        }
+        return input;
+    }
 }
