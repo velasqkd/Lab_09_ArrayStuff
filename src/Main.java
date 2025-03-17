ff88f7f83904d9a9fefdcb3fd6d2895f72f2dfcb
velasqkd <velasqkd@mail.uc.edu>
2025-03-16 20:31:37 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Random rnd = new Random();
+        int[] dataPoints = new int[100];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rnd.nextInt(100) + 1;
         }
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1) {
+                System.out.print(" | ");
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            sum += dataPoints[i];
+        }
+        double average = (double) sum / dataPoints.length;
+        System.out.println("The sum of the dataPoints array is: " + sum);
+        System.out.println("The average of the dataPoints array is: " + average);
+
+        int userInput = SafeInput.getRangedInt("Enter a value between 1 and 100: ", 1, 100);
+
+        int count = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userInput) {
+                count++;
+            }
+        }
+        System.out.println("The value " + userInput + " was found " + count + " times.");
+
+        int position = -1;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userInput) {
+                position = i;
+                break;
+            }
+        }
+        if (position != -1) {
+            System.out.println("The value " + userInput + " was found at index " + position);
+        } else {
+            System.out.println("The value " + userInput + " was not found in the array.");
+        }
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+        for (int i = 1; i < dataPoints.length; i++) {
+            if (dataPoints[i] < min) {
+                min = dataPoints[i];
+            }
+            if (dataPoints[i] > max) {
+                max = dataPoints[i];
+            }
+        }
+        System.out.println("The minimum value in the array is: " + min);
+        System.out.println("The maximum value in the array is: " + max);
+
+        System.out.println("Average of dataPoints is: " + getAverage(dataPoints));
+    }
+
+    public static double getAverage(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return (double) sum / values.length;
     }
 }
